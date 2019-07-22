6852a96786e1eb2760dae7e5be79299f3e07ca39
Esperanza González <esperanza@mechaphysis.org>
2019-07-22 17:29:24 +0200

    feat: define safe defaults

---
+++ constants.js
@@ -0,0 +1,8 @@
+/**
+ *  set of safe defaults to initialize variables
+ *  Freeze is used to avoid unintended mutations
+ *  */
+
+exports.EMPTY_OBJECT_READONLY = Object.freeze({});
+
+exports.EMPTY_ARRAY_READONLY = Object.freeze([]);
