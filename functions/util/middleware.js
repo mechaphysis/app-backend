07e090412437b82a2b413cccd539a9aa1ccd565e
Esperanza González <esperanza@mechaphysis.org>
2019-08-07 19:08:15 +0200

    refactor: rename data as snapshot for clarity

--- functions/util/middleware.js
+++ functions/util/middleware.js
@@ -15,18 +15,16 @@ exports.FBAuth = (req, resp, next) => {
     return admin.auth().verifyIdToken(idToken)
         .then(decodedToken => {
             req.user = decodedToken;
-            return db.collection('users')
-                .where('userId', '==', req.user.uid)
-                .limit(1)
-                .get()
+            return db
+              .collection('users').where("userId", "==", req.user.uid).limit(1).get()
         })
-        .then( data => {
-            req.user.handle = data.docs[0].data().handle
-            req.user.imageUrl = data.docs[0].data().imageUrl 
+        .then( snapShot => {
+            req.user.handle = snapShot.docs[0].data().handle
+            req.user.imageUrl = snapShot.docs[0].data().imageUrl 
             return next()
         })
         .catch(error => {
             console.error('Something went wrong: ', error)
-            resp.status(403).json(error)
+            return resp.status(403).json(error)
         })
 }
